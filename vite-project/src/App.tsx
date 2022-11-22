d72549b32b5f8d2f1dac4fb037f89b19d568d430
elias-cribas <elias.coelho92004@gmail.com>
2022-11-22 11:05:42 -0200

    Created navbar

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,12 +6,26 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="text">
-      <div className='monkey-img'>
-        <img src=".\src\images\monkey.png" alt="" />
+    <div className="container">
+      <div className="nav-container">
+        <div className="logo">
+          <img src=".\src\images\monkey.png" alt="" />
+        </div>
+        <ul className="nav-area">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Monkey</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
       </div>
+     <div className="welcome-text">
+       <h1>We are monkey</h1>
+       <a href="#">Contact us</a>
+     </div>
     </div>
+
   )
 }
 
 export default App
+
